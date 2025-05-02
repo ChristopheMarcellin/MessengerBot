50e994c1bab59bff0b4bd4c4ce7046cf2ae08568
Christophe Marcellin <christophemarcellin@hotmail.com>
2025-05-02 13:04:27 -0400

    f

--- modules/displayMap.js
+++ modules/displayMap.js
@@ -1,64 +1,68 @@
 module.exports = {
-  projectType: {
-    fr: { B: "Achat", S: "Vente", R: "Location", E: "Autre" },
-    en: { B: "Buying", S: "Selling", R: "Renting", E: "Other" }
-  },
-  intention: {
-    fr: { 1: "Acheter", 2: "Vendre", 3: "Louer", 4: "Autre raison" },
-    en: { 1: "Buy", 2: "Sell", 3: "Rent", 4: "Other reason" }
-  },
-  bedrooms: {
-    fr: {
-      1: "1 chambre ou plus",
-      2: "2 chambres ou plus",
-      3: "3 chambres ou plus",
-      4: "4 chambres ou plus",
-      5: "5 chambres ou plus"
+    projectType: {
+        fr: { B: "Achat", S: "Vente", R: "Location", E: "Autre" },
+        en: { B: "Buying", S: "Selling", R: "Renting", E: "Other" }
     },
-    en: {
-      1: "1 bedroom or more",
-      2: "2 bedrooms or more",
-      3: "3 bedrooms or more",
-      4: "4 bedrooms or more",
-      5: "5 bedrooms or more"
-    }
-  },
-  bathrooms: {
-    fr: {
-      1: "1 salle de bain ou plus",
-      2: "2 salles de bain ou plus",
-      3: "3 salles de bain ou plus"
+    intention: {
+        fr: { 1: "Acheter", 2: "Vendre", 3: "Louer", 4: "Autre raison" },
+        en: { 1: "Buy", 2: "Sell", 3: "Rent", 4: "Other reason" }
     },
-    en: {
-      1: "1 bathroom or more",
-      2: "2 bathrooms or more",
-      3: "3 bathrooms or more"
-    }
-  },
-  garage: {
-    fr: {
-      0: "Aucun garage",
-      1: "1 garage",
-      2: "2 garages",
-      3: "2 garages ou plus"
+    bedrooms: {
+        fr: {
+            1: "1 chambre ou plus",
+            2: "2 chambres ou plus",
+            3: "3 chambres ou plus",
+            4: "4 chambres ou plus",
+            5: "5 chambres ou plus"
+        },
+        en: {
+            1: "1 bedroom or more",
+            2: "2 bedrooms or more",
+            3: "3 bedrooms or more",
+            4: "4 bedrooms or more",
+            5: "5 bedrooms or more"
+        }
     },
-    en: {
-      0: "No garage",
-      1: "1 garage",
-      2: "2 garages",
-      3: "2+ garages"
-    }
-  },
-  parking: {
-    fr: {
-      0: "Pas de stationnement privé",
-      1: "Oui, stationnement privé"
+    bathrooms: {
+        fr: {
+            1: "1 salle de bain ou plus",
+            2: "2 salles de bain ou plus",
+            3: "3 salles de bain ou plus"
+        },
+        en: {
+            1: "1 bathroom or more",
+            2: "2 bathrooms or more",
+            3: "3 bathrooms or more"
+        }
     },
-    en: {
-      0: "No private parking",
-      1: "Yes, private parking"
+    garage: {
+        fr: {
+            0: "Aucun garage",
+            1: "1 garage",
+            2: "2 garages",
+            3: "2 garages ou plus"
+        },
+        en: {
+            0: "No garage",
+            1: "1 garage",
+            2: "2 garages",
+            3: "2+ garages"
+        }
+    },
+    parking: {
+        fr: {
+            0: "Pas de stationnement privé",
+            1: "Oui, stationnement privé"
+        },
+        en: {
+            0: "No private parking",
+            1: "Yes, private parking"
+        }
+    },
+    price: {
+        fr: {
+            1: "Moins de 200 000$"
+        }
     }
-  },
-  price: {
-    fr: {
-      1: "Moins de 200 000$",
+}    ;
+   
