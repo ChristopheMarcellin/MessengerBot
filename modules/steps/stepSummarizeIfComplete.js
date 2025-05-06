88ebda1e844bd7458551700e89fbfc30ce150512
Christophe Marcellin <christophemarcellin@hotmail.com>
2025-05-06 09:40:35 -0400

    f

--- modules/steps/stepSummarizeIfComplete.js
+++ modules/steps/stepSummarizeIfComplete.js
@@ -1,6 +1,7 @@
 const { sendMessage } = require('../messenger');
 const { buildSpecSummary } = require('../specEngine');
 const { allSpecsCollected } = require('../utils');
+const { allSpecsCollected } = require('../specEngine');
 
 async function stepSummarizeIfComplete({ senderId, session }) {
     if (!session.completedSpecs && allSpecsCollected(session) && session.specValues.projectType !== "?") {
