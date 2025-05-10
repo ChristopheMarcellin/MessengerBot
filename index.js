5076903e9dcf60b199abb94315f43e7f9bda89a0
Christophe Marcellin <christophemarcellin@hotmail.com>
2025-05-10 11:58:41 -0400

    f

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
@@ -91,4 +92,22 @@ app.post('/webhook', async (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`[INIT] Messenger test server running on port ${PORT}`);
+});
+*/
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    console.log('[SAFE MODE] Messenger request ignored.');
+    res.status(200).send('SAFE MODE');
+});
+
+app.get('/', (req, res) => {
+    res.send('Safe mode is active.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`[SAFE MODE] Server running on port ${PORT}`);
 });
