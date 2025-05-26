6c4e0e3b225111a22b5ce1cddff8c3923384a038
Christophe Marcellin <christophemarcellin@hotmail.com>
2025-05-26 10:23:11 -0400

    10h23

--- modules/director.js
+++ modules/director.js
@@ -1,5 +1,5 @@
 const { isValidAnswer, getProjectTypeFromNumber } = require('./specEngine');
-const { getSession } = require('../sessionStore');
+const { getSession } = require('./sessionStore');
 const {
     setProjectType,
     initializeSpecFields,
