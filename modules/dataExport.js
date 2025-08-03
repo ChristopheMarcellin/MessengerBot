8fb037973419c4874b7f0f0ae7f54267d59193c1
Christophe Marcellin <christophemarcellin@hotmail.com>
2025-08-03 16:06:38 -0400

    18h25

--- modules/dataExport.js
+++ modules/dataExport.js
@@ -1,22 +1,13 @@
 // modules/dataExport.js
-const fetch = require('node-fetch');
-
 const webhookUrl = 'https://script.google.com/macros/s/AKfycbxguRpiWDJHmqP153umdsXbZ_JlWLenlRXnH-vst12885H-Adse98OTm-A4NN-PFIZY/exec';
 
-/**
- * Ajoute un message (Q ou A) dans la colonne QnA
- * pour la ligne correspondant au senderId et timestamp le plus récent
- * @param {string} senderId - ID Messenger de l'utilisateur
- * @param {string} message - Texte du message
- * @param {string} type - "Q" (Question) ou "A" (Answer)
- */
 async function logQnA(senderId, message, type) {
     const payload = {
         senderId,
-        type,                // "Q" ou "A"
+        type,
         message,
         timestamp: new Date().toISOString(),
-        action: 'appendQnA'  // ← Indique au Apps Script qu’on veut mettre à jour la colonne QnA
+        action: 'appendQnA'
     };
 
     try {
